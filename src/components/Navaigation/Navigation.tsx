e10f2b1957c3fcf418e473caaff89573b199346b
SeieunYoo <isho48239@gmail.com>
2024-03-17 15:54:09 +0900

    style: 네비게이션 디자인 수정

--- src/components/Navaigation/Navigation.tsx
+++ src/components/Navaigation/Navigation.tsx
@@ -6,21 +6,23 @@ export const Navigation = () => {
   const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <nav className="flex justify-between p-[3rem]">
-      <Link href="/">
+      <Link href="/" className="flex gap-1">
         <Icon name="blog" color="black" />
-        <button className="text-5xl pb-10">seieun.blog</button>
+        <button className="text-5xl">Seieun.blog</button>
       </Link>
 
-      <a href="https://github.com/SeieunYoo" target="_blank">
-        <Icon src="https://img.icons8.com/ios-glyphs/30/github.png" width={30} height={30} />
-      </a>
-      <button onClick={toggleDarkMode}>
-        {darkMode ? (
-          <Icon name="light" color="black" width={30} height={30} />
-        ) : (
-          <Icon name="dark" color="black" width={30} height={30} />
-        )}
-      </button>
+      <div className="flex">
+        <a href="https://github.com/SeieunYoo" target="_blank" className="flex">
+          <Icon src="https://img.icons8.com/ios-glyphs/30/github.png" width={30} height={30} />
+        </a>
+        <button onClick={toggleDarkMode}>
+          {darkMode ? (
+            <Icon name="light" color="black" width={30} height={30} />
+          ) : (
+            <Icon name="dark" color="black" width={30} height={30} />
+          )}
+        </button>
+      </div>
     </nav>
   );
 };
