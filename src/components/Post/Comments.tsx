68f9414b01ed07404e0f10486d8c80a870467fb4
SeieunYoo <isho48239@gmail.com>
2024-03-23 13:40:36 +0900

    ✨ feat: 코멘트 다크모드 반영

--- src/components/Post/Comments.tsx
+++ src/components/Post/Comments.tsx
@@ -1,38 +1,21 @@
-import useDarkMode from "@/hooks/useDarkMode";
-import React, { useEffect, useRef } from "react";
-
-export default function Comments() {
-  const ref = useRef<any>(null);
-  const { darkMode } = useDarkMode();
-
-  const makeComments = () => {
-    const script = document.createElement("script");
-
-    script.src = "https://utteranc.es/client.js";
-    script.async = false;
-    script.setAttribute("repo", "SeieunYoo/seieun.blog");
-    script.setAttribute("issue-term", "title");
-
-    // browser theme에 따라서 Comments theme을 변경해줍니다.
-    if (darkMode) {
-      script.setAttribute("theme", "dark-blue");
-    } else {
-      script.setAttribute("theme", "github-light");
-    }
-    script.setAttribute("label", "blog-comment");
-    ref.current.appendChild(script);
-  };
-
-  const removeExistedComments = () => {
-    const existingScript = ref.current.querySelector(".utterances");
-    if (existingScript) {
-      existingScript.remove();
-    }
-  };
-  useEffect(() => {
-    makeComments();
-    removeExistedComments();
-  }, [darkMode]);
-
-  return <div ref={ref}></div>;
+function Comments() {
+  return (
+    <section
+      ref={(elem) => {
+        if (!elem) {
+          return;
+        }
+        const scriptElem = document.createElement("script");
+        scriptElem.src = "https://utteranc.es/client.js";
+        scriptElem.async = true;
+        scriptElem.setAttribute("repo", "SeieunYoo/seieun.blog");
+        scriptElem.setAttribute("issue-term", "title");
+        scriptElem.setAttribute("label", "blog-comment");
+        scriptElem.crossOrigin = "anonymous";
+        elem.appendChild(scriptElem);
+      }}
+    />
+  );
 }
+
+export default Comments;
