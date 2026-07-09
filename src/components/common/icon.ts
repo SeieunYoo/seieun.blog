0612a374604ea45d22ebbcd20191218d9df5ba7b
SeieunYoo <isho48239@gmail.com>
2026-07-09 23:21:31 +0900

    chore: 미사용 코드/의존성 정리
    - 어디서도 import되지 않는 죽은 파일 삭제: components/common/*,
      components/ui/provider.tsx(레거시 스캐폴드, app/providers.tsx로 대체됨),
      hooks/useDarkMode.ts, hooks/useParseDate.ts, components/Preview/*
    - 미사용 의존성 remark, remark-html 제거 (markdownToHtml.ts는 unified +
      remark-parse/remark-rehype 파이프라인만 사용)
    - SearchModal: query 변경 시 선택 인덱스를 리셋하던 useEffect를 렌더 중
      prev 비교로 교체해 불필요한 리렌더 제거
    - Providers/SearchProvider: 매 렌더마다 새 배열을 만드는 `= []` 기본값을
      모듈 상단 상수로 하드닝

--- src/components/common/icon.ts
+++
@@ -1,4 +0,0 @@
-export { default as blog } from "../../../public/icons/blog.svg";
-export { default as light } from "../../../public/icons/light.svg";
-export { default as dark } from "../../../public/icons/dark.svg";
-export { default as github } from "../../../public/icons/github.svg";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
 export * from "./Navaigation";
-export * from "./Preview";
